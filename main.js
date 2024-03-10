413a57ab57f11ff24977cee7164f991476579ca4
Shoaib Siddiq <159219257+student-shoaibsoomro@users.noreply.github.com>
2024-03-11 02:26:50 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+"use strict";
+let greeting = ['admin', 'Ahmed Raza', 'Nazar', 'Adeel', 'Areeb'];
+for (let i = 0; i < greeting.length; i++) {
+    if (greeting[i] === 'admin')
+        console.log('Hello admin, would you like to see a status report?');
+    else {
+        console.log(`Hello ${greeting[i]}: Thank you for logging in again.`);
+    }
+}
